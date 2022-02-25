eac411692ab1941ec34b92b3785e73600bd85b6f
Nils Kopshoff <nils@kopshoff.koeln>
2022-02-25 10:56:33 +0100

    REST CRUD schemata

---
+++ src/view/rest/mod.rs
@@ -0,0 +1,22 @@
+use rocket::serde::{json::Json};
+use crate::view::structures::{CashFlow, CashFlowDetail};
+
+pub fn create_cash_flow() -> Json<CashFlowDetail> {
+    Json(CashFlowDetail::empty())
+}
+
+pub fn read_cash_flow_all() -> Json<Vec<CashFlow>> {
+    Json(vec!())
+}
+
+pub fn read_cash_flow(_id: i32) -> Json<CashFlowDetail> {
+    Json(CashFlowDetail::empty())
+}
+
+pub fn update_cash_flow(_cash_flow: CashFlowDetail) -> Json<CashFlowDetail> {
+    Json(CashFlowDetail::empty())
+}
+
+pub fn delete_cash_flow(_id: i32) -> Json<CashFlowDetail> {
+    Json(CashFlowDetail::empty())
+}
